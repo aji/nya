5a82aa8ff83706c94c7e9813fb8fbf0dab62578c
Alex Iadicicco <alex@ajitek.net>
2015-04-06 21:56:33 -0700

    Implement multiple channels, external 'db', etc

--- nya.py
+++ nya.py
@@ -3,15 +3,35 @@ import urllib
 import time
 import traceback
 
-USERS = [
-    'theta4',
-    'CorgiDude',
-    'therealelizacat',
-    'alyxw',
-    'foxiepaws',
-    ]
+CONFPATH = 'nya.json'
+ON_CONF_CHANGED = []
 
-BUFFER = 'interlinked,#nya'
+CONF = {
+    'users': [
+        ],
+    }
+
+def load_conf():
+    global CONF
+
+    try:
+        with open(CONFPATH, 'r') as f:
+            CONF = json.load(f)
+    except IOError:
+        pass
+
+    conf_changed()
+
+def save_conf():
+    try:
+        with open(CONFPATH, 'w') as f:
+            json.dump(CONF, f)
+    except Exception:
+        traceback.print_exc()
+
+def conf_changed():
+    for cb in ON_CONF_CHANGED:
+        cb()
 
 import weechat
 
@@ -59,9 +79,9 @@ def url_finished(cb, command, rc, out, err):
     try:
         URL_REQUEST_TMP[cb]['cb'](URL_REQUEST_TMP[cb]['data'])
     except Exception as e:
-        weechat.prnt(MUSIC, '  !! {}'.format(str(e)))
+        weechat.prnt('', '  !! {}'.format(str(e)))
         for ln in traceback.format_exc().split('\n'):
-            weechat.prnt(MUSIC, ln)
+            weechat.prnt('', ln)
     del URL_REQUEST_TMP[cb]
     return weechat.WEECHAT_RC_OK
 def get_data(url, cb):
@@ -93,8 +113,9 @@ class Track(object):
         return 'Track({} by {})'.format(self.name, self.artist)
 
 class User(object):
-    def __init__(self, lastfm_name):
+    def __init__(self, lastfm_name, buffers):
         self.lastfm_name  = lastfm_name
+        self.buffers      = buffers
 
         self.last_track   = None
         self.last_poll    = None
@@ -141,7 +162,7 @@ def get_video(track, on_complete):
 def do_poll(u, on_complete):
     def request_completed(ok, tracks, u):
         if not ok:
-            weechat.prnt(MUSIC, '  !! #{}: {}'.format(
+            weechat.prnt('', '  !! #{}: {}'.format(
                     tracks[u'error'], tracks[u'message']))
             return
         i = -1
@@ -159,45 +180,129 @@ def do_poll(u, on_complete):
         if u.last_track is not None:
             if u.last_track.name == tracks[i].name:
                 return
-        weechat.prnt(MUSIC, '{}: {}'.format(repr(u), repr(tracks[i])))
+        weechat.prnt('', '{}: {}'.format(repr(u), repr(tracks[i])))
         u.last_track = tracks[i]
         on_complete(u)
     get_tracks(u, request_completed)
 
 def on_one_fire(data, remaining):
-    global MUSIC
-    MUSIC = weechat.info_get('irc_buffer', BUFFER)
     def on_complete(u):
         def got_video_id(vid):
-            weechat.command(MUSIC, '/say \0033 {} now listening to "{}" by {}{}'
+            msg = ('/say \0033 {} now listening to "{}" by {}{}'
                 .format(u.lastfm_name, u.last_track.name, u.last_track.artist,
                 '' if vid is None else ': http://youtu.be/{}'.format(vid))
                 )
+            for b in u.buffers:
+                buf = weechat.info_get('irc_buffer', b)
+                if buf:
+                    weechat.command(buf, msg)
         get_video(u.last_track, got_video_id)
-    u = ALL_USERS[int(remaining)]
-    do_poll(u, on_complete)
+    if int(remaining) < len(ALL_USERS):
+        u = ALL_USERS[int(remaining)]
+        do_poll(u, on_complete)
     return weechat.WEECHAT_RC_OK
 
 def on_timer_fire(data, remaining):
-    global MUSIC
-    MUSIC = weechat.info_get('irc_buffer', BUFFER)
     if not LASTFM_API_KEY():
-        weechat.prnt(MUSIC, '!!!! no lastfm key set')
+        weechat.prnt('', '!!!! no lastfm key set')
         return weechat.WEECHAT_RC_ERROR
     if not YOUTUBE_API_KEY():
-        weechat.prnt(MUSIC, 'warning: no youtube key')
-    weechat.hook_timer(900, 0, len(ALL_USERS), 'on_one_fire', '')
+        weechat.prnt('', 'warning: no youtube key')
+    if len(ALL_USERS) > 0:
+        weechat.hook_timer(900, 0, len(ALL_USERS), 'on_one_fire', '')
     weechat.hook_timer((max(len(ALL_USERS), 5) + 2) * 1000,
                        0, 1, 'on_timer_fire', '')
     return weechat.WEECHAT_RC_OK
 
-weechat.register('nya', 'aji', '1.0', 'MIT', 'nya!', '', '')
+ALL_USERS = []
+def initialize_users():
+    global ALL_USERS
+    ALL_USERS = [User(u['lastfm'], u['buffers']) for u in CONF['users']]
+ON_CONF_CHANGED.append(initialize_users)
 
-ALL_USERS = [User(u) for u in USERS]
+def do_follow(bufname, user):
+    for u in CONF['users']:
+        if u['lastfm'] == user:
+            if bufname in u['buffers']:
+                return False
+            u['buffers'].append(bufname)
+            return True
+    CONF['users'].append({
+        'lastfm': user,
+        'buffers': [bufname],
+        })
+    return True
+
+def do_unfollow(bufname, user):
+    do_remove = None
+    for u in CONF['users']:
+        if u['lastfm'] == user:
+            if bufname not in u['buffers']:
+                return False
+            u['buffers'].remove(bufname)
+            if len(u['buffers']) == 0:
+                do_remove = u
+                break
+            return True
+    if do_remove is not None:
+        CONF['users'].remove(do_remove)
+        return True
+    return False
+
+def get_following(bufname):
+    users = []
+    for u in CONF['users']:
+        if bufname in u['buffers']:
+            users.append(u)
+    return users
+
+def run_command(net, chan, args):
+    bufname = '{},{}'.format(net, chan)
+    buf = weechat.info_get('irc_buffer', bufname)
+    if args[0] == 'follow':
+        if do_follow(bufname, args[1]):
+            conf_changed()
+            save_conf()
+            weechat.command(buf, '/say \0032now following {}'.format(args[1]))
+        else:
+            weechat.command(buf, '/say \0032already following {}'.format(args[1]))
+    if args[0] == 'unfollow':
+        if do_unfollow(bufname, args[1]):
+            conf_changed()
+            save_conf()
+            weechat.command(buf, '/say \0032unfollowed {}'.format(args[1]))
+        else:
+            weechat.command(buf, '/say \0032not following {}'.format(args[1]))
+    if args[0] == 'following':
+        users = get_following(bufname)
+        if len(users) == 0:
+            weechat.command(buf, '/say \0032not following anybody!')
+        else:
+            weechat.command(buf, '/say \0032following: {}'.format(', '.join(
+                u['lastfm'] for u in users)))
+
+def try_command(data, signal, signal_data):
+    sig = weechat.info_get_hashtable('irc_message_parse', {
+        'message': signal_data})
+    args = sig['arguments'].split(' :', 1)
+    tail = ''
+    if len(args) > 1:
+        tail = args[1]
+    args = args[0].split()
+    nick = weechat.info_get('irc_nick', '')
+    tail = tail.lstrip()
+    if not tail.startswith(nick):
+        return weechat.WEECHAT_RC_OK
+    run_command(signal.split(',')[0], sig['channel'], tail.split()[1:])
+    return weechat.WEECHAT_RC_OK
+
+weechat.register('nya', 'aji', '1.0', 'MIT', 'nya!', '', '')
 
 for option, dfl in OPTIONS.items():
     if not weechat.config_is_set_plugin(option):
         weechat.config_set_plugin(option, dfl)
 
+load_conf()
+weechat.hook_signal('*,irc_in_privmsg', 'try_command', '')
 on_timer_fire('', '') #HAX ROFL
-weechat.command(MUSIC, '/say \0032nya (re)loaded')
+weechat.prnt('', '(nya): nya (re)loaded')
